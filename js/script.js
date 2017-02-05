89ff2fad608fe2b6e7eb9e0ada867dbb75964907
SAYDUR <saydur_93@live.com>
2017-02-05 18:43:24 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "fade",
+    controlNav: false,
+    controlsContainer: $(".custom-controls-container"),
+    customDirectionNav: $(".custom-navigation a")
+  });
+});
