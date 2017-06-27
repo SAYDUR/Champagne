e0a41b8a59128c0a1f217807fdb677e6ad00c790
SAYDUR <saydur_93@live.com>
2017-06-27 20:02:44 +0100

    Fully Cleaned File

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 $(window).load(function() {
-  $('.flexslider').flexslider({
-    animation: "fade",
-    controlNav: false,
-    controlsContainer: $(".custom-controls-container"),
-    customDirectionNav: $(".custom-navigation a")
-  });
+
+
+
 });
